f35c0612f4a13b2ab3a2b75f6cf99c68ba28ee2b
Long Zheng <long.zheng@gmail.com>
2020-09-12 18:35:21 +1000

    create-react-app and material-ui stub

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+function App() {
+  return (
+    <CssBaseline>
+      
+    </CssBaseline>
+  );
+}
+
+export default App;
