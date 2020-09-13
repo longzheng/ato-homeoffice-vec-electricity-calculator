ebc80edbb226f6417b9541e80606c6d0dc3eea2d
Long Zheng <long.zheng@gmail.com>
2020-09-13 13:19:58 +1000

    Updates

---
+++ precache-manifest.3f75d27e3d3d09f27933bbb41ecf477b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a90a538d91bb66c6b91dfc8241a2a4",
+    "url": "/ato-homeoffice-vec-electricity-calculator/index.html"
+  },
+  {
+    "revision": "6e21e0d28b7647064c70",
+    "url": "/ato-homeoffice-vec-electricity-calculator/static/js/2.024c1f2d.chunk.js"
+  },
+  {
+    "revision": "8d033527bfde269db1e0fb1aca3c57e8",
+    "url": "/ato-homeoffice-vec-electricity-calculator/static/js/2.024c1f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d2561be5c2a997c70d",
+    "url": "/ato-homeoffice-vec-electricity-calculator/static/js/main.d4dccc2f.chunk.js"
+  },
+  {
+    "revision": "aae846e16461a66a0f9a",
+    "url": "/ato-homeoffice-vec-electricity-calculator/static/js/runtime-main.ca989e34.js"
+  }
+]);
